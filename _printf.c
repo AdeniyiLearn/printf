3568873bb4dc07033744d4246ce1f237ff9a445b
SEUN <adeniyi.learn@gmail.com>
2023-01-26 19:37:00 -0800

    lll

--- _printf.c
+++ _printf.c
@@ -14,47 +14,55 @@ int _printf(const char *format, ...)
 	va_list arg;
 
 	va_start(arg, format);
-	for (count = 0; count < len; count++)
+	if (*format == '\0')
 	{
-		/*print string without formats*/
-		if (*(format + count) != '%')
-		{
-			_putchar(*(format + count));
-			pc1++;
-		}
-		/*encounter with % and subsequents specifiers*/
-		else
-		{
-		count += 1;
-			if (*(format + (count)) == 'c')
-			{
-				num = va_arg(arg, int);
-				if (num == '\0')
-				{
-					continue;
-				}
-				else
-				{
-					_putchar(num);
-				}
-				pc2++;
-			}
-			else if (*(format + (count)) == 's')
-			{
-				string = va_arg(arg, char *);
-				if (string == NULL)
-				{
-					continue;
-				}
-				else
-				{
-					string_put(string);
-				}
-				pc3++;
-			}
-		}
-
+		return(1);
 	}
-	va_end(arg);
+	else
+	{
+		for (count = 0; count < len; count++)
+		{
+			/*print string without formats*/
+			if (*(format + count) != '%')
+			{
+				_putchar(*(format + count));
+				pc1++;
+			}
+			/*encounter with % and subsequents specifiers*/
+			else
+			{
+			count += 1;
+				if (*(format + (count)) == 'c')
+				{
+					num = va_arg(arg, int);
+					if (num == '\0')
+					{
+						continue;
+					}
+					else
+					{
+						_putchar(num);
+					}
+					pc2++;
+				}
+				else if (*(format + (count)) == 's')
+				{
+					string = va_arg(arg, char *);
+					if (string == NULL)
+					{
+						continue;
+					}
+					else
+					{
+						string_put(string);
+					}
+					pc3++;
+				}
+			}
+
+	
+		}
+		va_end(arg);
+	}
 	return (pc1 + pc2 + pc3);
 }
